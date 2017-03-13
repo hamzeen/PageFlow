05e50110ca81376c071094b62d71b99ae85a1e13
hamzeen.hameem <Hamzeen.Hameem@sc.com>
2017-03-13 23:38:19 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,18 @@
+/**
+ * Holds page data
+ * Author: Hamzeen. H.
+ * Version: 0.1
+ */
+
+var _items = [
+		{ id: 1, title: 'Home', 
+			content: '<p>Welcome to motion based navigation</p>', 
+			slug: 'home' },
+
+		{ id: 2, title: 'About', 
+			content: '<p>All image processing on this page is done with Javascript.</p>', 
+			slug: 'lahiru' },
+		
+		{ id: 3, title: 'Conact', 
+			content: '<p>If you find this interesting, please get in touch with us. We will be delighted to carry this work forward.</p>', 
+			slug: 'contact' }];
